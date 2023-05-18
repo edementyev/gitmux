0093e66d49d5bd2f8c58b8575d073c6099b95f1a
Egor Dementyev <bespamiatny@gmail.com>
2023-05-18 12:06:14 +0300

    feat: repo markers

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,8 @@ struct ConfigEntry<'a> {
     depth: u8,
     #[serde(default)]
     include_all: bool,
+    #[serde(default)]
+    markers: Vec<&'a str>,
 }
 
 impl<'a> Default for ConfigEntry<'a> {
@@ -27,6 +29,7 @@ impl<'a> Default for ConfigEntry<'a> {
             exclude: vec![],
             depth: 0,
             include_all: false,
+            markers: vec![],
         }
     }
 }
@@ -47,6 +50,21 @@ fn expand(path: &str) -> String {
 static APP_NAME: &str = "tmux-repoizer";
 static CONFIG_PATH: &str = "/home/yev/.config/tmux-repoizer/config.json";
 
+static MARKERS: [&str; 2] = [".git", "Cargo.toml"];
+static IGNORE_DIRS: [&str; 11] = [
+    "node_modules",
+    "venv",
+    "bin",
+    "target",
+    "debug",
+    "src",
+    "test",
+    "tests",
+    "lib",
+    "docs",
+    "pkg",
+];
+
 fn main() {
     // parse cli args
     let cmd = clap::Command::new(APP_NAME).arg(
@@ -66,60 +84,37 @@ fn main() {
         serde_json::from_str(file.as_str()).expect("error parsing config.json");
     // println!("{:?}", config);
     // println!("{}", expand("${HOME}/${HOME}/dd"));
-    let mut repos = vec![];
-    let ignore_dirs: Vec<&str> = vec![
-        "node_modules",
-        "venv",
-        "bin",
-        "target",
-        "debug",
-        "src",
-        "test",
-        "tests",
-        "lib",
-        "docs",
-        "pkg",
-    ];
-    for mut entry in config {
-        entry.exclude.extend(&ignore_dirs);
-        for path in entry.include {
-            descend(
-                expand(path).as_str(),
-                1,
-                entry.depth,
-                &mut repos,
-                &entry.exclude,
-                entry.include_all,
-            );
+    let mut output_vec = vec![];
+    for mut config_entry in config {
+        config_entry.exclude.extend(&IGNORE_DIRS);
+        config_entry.markers.extend(&MARKERS);
+        for path in &config_entry.include {
+            descend(expand(path).as_str(), 1, &mut output_vec, &config_entry);
         }
     }
     let mut out = stdout();
     let mut output: String = "".to_string();
     // println!("{:?}", repos);
-    for r in repos {
+    for r in output_vec {
         output += (r + "\n").as_str();
     }
     out.write_fmt(format_args!("{}", output))
         .expect("error writing to stdout");
 }
 
-fn descend(
-    path: &str,
-    depth: u8,
-    max_depth: u8,
-    output: &mut Vec<String>,
-    ignore_dirs: &Vec<&str>,
-    include_all: bool,
-) -> bool {
-    if max_depth != 0 && depth > max_depth {
+fn descend(path: &str, depth: u8, output: &mut Vec<String>, config: &ConfigEntry) -> bool {
+    if config.depth != 0 && depth > config.depth {
         return false;
     }
-    let mut include_this_path = depth == 1 || include_all;
-    if let Ok(_) = std::fs::metadata(path.to_string() + "/.git") {
-        // println!("is git {}", path);
-        output.push(path.to_string());
-        return true;
-    } else if let Ok(mut iter) = read_dir(path) {
+    let mut include_this_path = depth == 1 || config.include_all;
+    for marker in &config.markers {
+        if let Ok(_) = std::fs::metadata(path.to_string() + "/" + marker) {
+            // println!("is git {}", path);
+            output.push(path.to_string());
+            return true;
+        }
+    }
+    if let Ok(mut iter) = read_dir(path) {
         let mut next = iter.next();
         let mut children = vec![];
         while let Some(Ok(ref dir_entry)) = next {
@@ -128,21 +123,14 @@ fn descend(
                 .to_str()
                 .expect("not utf8 string")
                 .to_string();
-            if is_valid_dir(&dir_entry, &name, &ignore_dirs) {
+            if is_valid_dir(&dir_entry, &name, &config.exclude) {
                 // descend further
                 children.push(String::from(dir_entry.path().to_str().expect("path err")));
             }
             next = iter.next();
         }
         for child in children {
-            if descend(
-                child.as_str(),
-                depth + 1,
-                max_depth,
-                output,
-                ignore_dirs,
-                include_all,
-            ) {
+            if descend(child.as_str(), depth + 1, output, config) {
                 include_this_path = true;
             };
         }
